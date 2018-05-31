8a42cb2fbbbc09b322606f61c0dc277d588344eb
SUSTACHE <ryansustache@yahoo.com>
2018-05-31 10:12:53 -0400

    test

--- reSSNBot.py
+++ reSSNBot.py
@@ -14,7 +14,7 @@ import time
 bot = commands.Bot(command_prefix= ".")
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
-def load_opus_lib(opus_libs=OPUS_LIBS):
+async def load_opus_lib(opus_libs=OPUS_LIBS):
     if opus.is_loaded():
         return True
 
