c754b45830261dbab029b6d46ecacc79df6702fe
SSNDemon <ryansustache@yahoo.com>
2018-06-09 20:36:13 -0400

    fixed errors

--- reSSNBot.py
+++ reSSNBot.py
@@ -137,8 +137,8 @@ async def meme(ctx):
     player.start()
 
 @bot.command(pass_context=True)
-async def hug(ctx, user: discord.mention):
-    await bot.send_message(ctx.message.channel,"{} gave hugs to {}!".format(ctx.message.author.mention, user) )
+async def hug(ctx, user: discord.Member):
+    await bot.send_message(ctx.message.channel,"{} gave hugs to {}!".format(ctx.message.author.mention, user.mention) )
 
 
 
